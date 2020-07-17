517bac47028c00583f3dfd67a97a7edd17b3dfbe
William Rosales <williamrm.96@gmail.com>
2020-07-17 00:12:34 -0600

    Commit inicial: Controlador categoria

---
+++ backend/backend/AplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using backend.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend
+{
+    public class AplicationDbContext : DbContext
+    {
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Categoria> Categoria { get; set; }
+    }
+}
---
+++ backend/backend/Migrations/20200717061028_Inicial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace backend.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Categoria",
+                type: "varchar(100)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(120)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Categoria",
+                type: "varchar(120)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)");
+        }
+    }
+}
